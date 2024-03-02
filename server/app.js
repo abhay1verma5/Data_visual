8cc702ba03eedc7ef8745bf0d328479f192bf38e
abhay1verma5 <abhaykumarverma910@gmail.com>
2024-03-03 02:39:16 +0530

    Your commit message here

--- server/app.js
+++ server/app.js
@@ -3,31 +3,18 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const apiRoutes = require('./routes/api');
 require('dotenv').config(); 
-const {data}=require("./data")
-const {adddata}=require("./controllers/adddata")
+const { data } = require("./data");
+const { adddata } = require("./controllers/adddata");
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
+mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('MongoDB connected');
-
-  // Your initialization logic here, such as starting your server or running some setup tasks
-
-}).catch(err => {
-  console.error(process.env.MONGODB_URI, 'Error connecting to MongoDB: ', err);
-});
-
-//save data to mongodb calling  adddata controller
-adddata(data)
-
+// Save data to MongoDB by calling adddata controller
+adddata(data);
 
 // Routes
 app.use('/api', apiRoutes);
